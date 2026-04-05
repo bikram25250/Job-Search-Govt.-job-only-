8daaf6e24c88a0fbdd4f879895b9b779f1d25c98
bikram25250 <bkrmhldr69@gmail.com>
2026-04-05 20:31:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,4 +13,16 @@ function closeSidebar() {
   overlay.classList.remove('active');
   menuBtn.style.display = 'block';
 }
+// Signup page hide & show password
+function togglePassword(fieldId, element) {
+  const input = document.getElementById(fieldId);
+  const icon = element.querySelector('i');
 
+  if (input.type === 'password') {
+    input.type = 'text';
+    icon.classList.replace('fa-eye', 'fa-eye-slash');
+  } else {
+    input.type = 'password';
+    icon.classList.replace('fa-eye-slash', 'fa-eye');
+  }
+}
