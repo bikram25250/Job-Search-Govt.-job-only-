f67ed2b9898baff274a80f6aed0505f9ae7e9258
bikram25250 <bkrmhldr69@gmail.com>
2026-04-09 21:44:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -42,3 +42,18 @@ signupForm.addEventListener("submit", function(e) {
     window.location.href = "Success_Message.html";
   }, 2000);
 });
+//Signin page part
+ function togglePassword() {
+      const password = document.getElementById("password");
+      const eyeIcon = document.getElementById("eyeIcon");
+
+      if (password.type === "password") {
+        password.type = "text";
+        eyeIcon.classList.remove("fa-eye");
+        eyeIcon.classList.add("fa-eye-slash");
+      } else {
+        password.type = "password";
+        eyeIcon.classList.remove("fa-eye-slash");
+        eyeIcon.classList.add("fa-eye");
+      }
+    }
