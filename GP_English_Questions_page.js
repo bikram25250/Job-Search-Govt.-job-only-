9579456eb7026e39b7ceae5d3bae14a7e70b37fd
bikram25250 <bkrmhldr69@gmail.com>
2026-06-01 21:14:49 +0530

    Add files via upload

---
+++ GP_English_Questions_page.js
@@ -0,0 +1,68 @@
+const examInfo = {
+    exam: "Gram Panchayat 2026",
+    subject: "English",
+    time:10
+};
+
+/* =========================
+   QUESTIONS
+========================= */
+
+const questions = [
+
+    {
+        question: "Choose the synonym of Brave",
+        options: ["Coward", "Courageous", "Weak", "Lazy"],
+        answer: 1
+    },
+
+    {
+        question: "Antonym of Ancient",
+        options: ["Old", "Historic", "Modern", "Traditional"],
+        answer: 2
+    },
+
+    {
+        question: "Choose correct spelling",
+        options: ["Enviroment", "Environment", "Envirnment", "Enviornment"],
+        answer: 1
+    },
+
+    {
+        question: "Choose synonym of Happy",
+        options: ["Sad", "Joyful", "Weak", "Angry"],
+        answer: 1
+    },
+
+    {
+        question: "Choose antonym of Victory",
+        options: ["Success", "Defeat", "Achievement", "Win"],
+        answer: 1
+    },
+
+     {
+        question: "Choose antonym of Victory",
+        options: ["Success", "Defeat", "Achievement", "Win"],
+        answer: 1
+    },
+
+    
+     {
+        question: "Choose antonym of Victory",
+        options: ["Success", "Defeat", "Achievement", "Win"],
+        answer: 1
+    },
+
+     {
+        question: "Choose antonym of Victory",
+        options: ["Success", "Defeat", "Achievement", "Win"],
+        answer: 1
+    },
+
+     {
+        question: "Choose antonym of Victory",
+        options: ["Success", "Defeat", "Achievement", "Win"],
+        answer: 1
+    }
+
+];
