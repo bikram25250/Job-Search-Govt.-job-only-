9ee17aeeb9577680445413a93b7cf5bc1c61e636
bikram25250 <bkrmhldr69@gmail.com>
2026-06-02 20:21:39 +0530

    Update GP_English_Questions_page.js

--- GP_English_Questions_page.js
+++ GP_English_Questions_page.js
@@ -1,7 +1,7 @@
 const examInfo = {
     exam: "Gram Panchayat 2026",
     subject: "English",
-    time:1
+    time:2
 };
 
 /* =========================
@@ -10,60 +10,122 @@ const examInfo = {
 
 const questions = [
 
+    // {
+    //     question: "Choose the alternative which is closely resembles the mirror image of the given combination",
+    //     image:"Images/Reasoning/Reasoning_1.jpeg",
+    //     options: ["Option A", "Option B", "Option C", "Option D"],
+    //     answer: 1
+    // },
+
     {
-        question: "Choose the alternative which is closely resembles the mirror image of the given combination",
-        image:"Reasoning_1.jpeg",
-        options: ["Option A", "Option B", "Option C", "Option D"],
+        question: "What is the correct synonym of 'Obstinate' ?",
+        options: ["Stubborn", "Pretty", "Silly", "Clever"],
+        answer: 0
+    },
+
+    {
+        question: "Choose the correct synonym of 'Alert' ?",
+        options: ["Hostile", "Watchful", "Brave", "Quick"],
         answer: 1
     },
 
     {
-        question: "Antonym of Ancient",
-        options: ["Old", "Historic", "Modern", "Traditional"],
-        answer: 2
+        question: "Choose the correct synonym of 'Accede' ?",
+        options: ["Consent", "Angry", "Assess", "Proceed"],
+        answer: 0
     },
 
     {
-        question: "Choose correct spelling",
-        options: ["Enviroment", "Environment", "Envirnment", "Enviornment"],
-        answer: 1
-    },
-
-    {
-        question: "Choose synonym of Happy",
-        options: ["Sad", "Joyful", "Weak", "Angry"],
-        answer: 1
-    },
-
-    {
-        question: "Choose antonym of Victory",
-        options: ["Success", "Defeat", "Achievement", "Win"],
+        question: "Voice Change (Active to Passive) - 'Sita loves savitri' ",
+        options: ["Savitri loved by sita", 
+                  "Savitri is loved by Sita", 
+                  "Savitri is loved to Sita", 
+                  "Savitri is loved to Sita"],
         answer: 1
     },
 
      {
-        question: "Choose antonym of Victory",
-        options: ["Success", "Defeat", "Achievement", "Win"],
-        answer: 1
+        question: "Voice Change (Active to Passive) - 'The mason is building the wall'",
+        options: ["The wall built by the mason", 
+                  "The wall is being built by mason", 
+                  "The wall is built by the mason", 
+                  "The wall is being built by the mason"],
+        answer: 3
     },
 
     
      {
-        question: "Choose antonym of Victory",
-        options: ["Success", "Defeat", "Achievement", "Win"],
+        question: "Choose the correct antonym 'Omit' ?",
+        options: ["Exclude", "Include", "Undertake", "Add"],
         answer: 1
     },
 
      {
-        question: "Choose antonym of Victory",
-        options: ["Success", "Defeat", "Achievement", "Win"],
-        answer: 1
+        question: "What is the correct antonym of 'Integration' ?",
+        options: ["Unit", "Synthesis", "Linking", "Fragmentation"],
+        answer: 3
     },
 
      {
-        question: "Choose antonym of Victory",
-        options: ["Success", "Defeat", "Achievement", "Win"],
+        question: "Choose the correct punctuation",
+        options: ["England France and Italy,formed an aliance", 
+                  "England,France and Italy formed an aliance", 
+                  "England France,and,Italy formed an aliance", 
+                  "England France and Italy formed,an,aliance"],
         answer: 1
+    },
+
+    {
+        question: "Choose the correct punctuation",
+        options: ["He lost,lands,money,reputation and friend's", 
+                  "He lost lands money reputation and friend's", 
+                  "He lost lands money,reputation and friend's", 
+                  "He lost lands,money,reputation and friend's"],
+        answer: 3
+    },
+
+    {
+        question: "Choose the correct punctuation",
+        options: ["West Bengal is surrounded by Odisha,Bihar,Sikkim,Assam etc.", 
+                  "West Bengal is surrounded by Odisha Bihar,Sikkim,Assam etc.", 
+                  "West,Bengal is surrounded by Odisha,Bihar,Sikkim,Assam etc.", 
+                  "West . Bengal is surrounded by Odisha,Bihar,Sikkim,Assam etc."],
+        answer: 0
+    },
+
+     {
+        question: "Choose the correct spelling",
+        options: ["Excution", "Excitment", "Expedition", "Extrection"],
+        answer: 2
+    },
+
+     {
+        question: "Choose the correct spelling",
+        options: ["Introvert", "Introvrt", "Intraovert", "Intreovert"],
+        answer: 0
+    },
+
+     {
+        question: "Choose the correct spelling",
+        options: ["Discer", "Discerrn", "Discern", "None of These"],
+        answer: 2
+    },
+
+    {
+        question: "Choose the correct spelling",
+        options: ["Visionery", "Viseonary", "Visionary", "None of These"],
+        answer: 2
+    },
+
+    {
+        question: "Find the error - 'Knowledge of (A)/at least two languages (B)/are required to pass the examination (C)/No error (D)'",
+        options: ["A", "B", "C", "D"],
+        answer: 2
+    },
+
+     {
+        question: "Find the error - 'Gowri told me (A)/her name after (B)/he left (C)/B and C (D)'",
+        options: ["A", "B", "C", "D"],
+        answer: 3
     }
-
 ];
