765ca16e1dfc0b0deec6337b0b129844dde82de9
bikram25250 <bkrmhldr69@gmail.com>
2026-06-09 18:23:34 +0530

    Update GP_English_Questions_page.js

--- GP_English_Questions_page.js
+++ GP_English_Questions_page.js
@@ -1,7 +1,7 @@
 const examInfo = {
     exam: "Gram Panchayat 2026",
     subject: "English",
-    time:2
+    time: 45
 };
 
 /* =========================
@@ -127,5 +127,109 @@ const questions = [
         question: "Find the error - 'Gowri told me (A)/her name after (B)/he left (C)/B and C (D)'",
         options: ["A", "B", "C", "D"],
         answer: 3
+    },
+
+    {
+        question: "Find the error - 'No sooner had the hockey match started (A)/when it began (B)/to rain (C)/No error (D)'",
+        options: ["A", "B", "C", "D"],
+        answer: 1
+    },
+
+    {
+        question: "Find the error - 'My sister (A)/has read (B)/pages after pages of the bible(C)/No error (D)'",
+        options: ["A", "B", "C", "D"],
+        answer: 2
+    },
+
+     {
+        question: "Fill in the blanks - 'My book is the new one _______ is the torn one'",
+        options: ["Your", "The book of you", "Yours", "The book your"],
+        answer: 2
+    },
+
+     {
+        question: "Fill in the blanks - 'They needn't worry ________ ?'",
+        options: ["Isn't", "Doesn't it", "Don't it", "Need they"],
+        answer: 3
+    },
+
+    {
+        question: "Fill in the blanks - 'He is different alike __________  praise and blame'",
+        options: ["In", "To", "For", "About"],
+        answer: 1
+    },
+
+    {
+        question: "Fill in the blanks - 'Ashoka's accession _________ the throme was celebrated with great promp'",
+        options: ["For", "With", "Against", "To"],
+        answer: 3
+    },
+
+    {
+        question: "Idiom Meaning - 'I have <u> other fish to fry </u>'",
+        options: ["To cook the food", "All of the above", "Some important work to attend ", "To attend the friends"],
+        answer: 2
+    },
+
+    {
+        question: "Idiom Meaning - 'A close shave'",
+        options: ["A narrow escape from danger", "Stubble", "Very short hair", "Hairless skin"],
+        answer: 0
+    },
+
+    {
+        question: "Idiom Meaning - 'His argument doesn't <u> hold water </u>'",
+        options: ["To have effect", "To influence", "To check the flow of water", "Sound logical fact"],
+        answer: 3
+    },
+
+    {
+        question: "Choose the correct Preposition - 'He is _________ the phone right now'",
+        options: ["At", "On", "To", "In"],
+        answer: 1
+    },
+
+    {
+        question: "Choose the correct Preposition - 'I have no desire _________ fame'",
+        options: ["Of", "For", "In", "To"],
+        answer: 1
+    },
+
+    {
+        question: "Choose the correct Preposition - 'There is an exception __________ every rule'",
+        options: ["To", "In", "For", "Up"],
+        answer: 0
+    },
+
+    {
+        question: "Choose the correct Article - 'Rama is ________ MA in english'",
+        options: ["Of", "In", "An", "To"],
+        answer: 2
+    },
+
+    {
+        question: "Choose the correct Preposition - 'The sun rises ________ the sky'",
+        options: ["At", "In", "On", "To"],
+        answer: 1
+    },
+
+    {
+        question: "Choose the correct Preposition - 'Netaji Subhas was born ______ 23rd january ______ the year 1897'",
+        options: ["On", "At", "In", "'On' and 'In' both"],
+        answer: 3
+    },
+
+     {
+        question: "Choose the correct Articles - 'Honestly is ________ best policy'",
+        options: ["A", "In", "An", "The"],
+        answer: 3
+    },
+
+     {
+        question: "Choose the correct Vocabulary - Nearest meaning of 'Authentic'",
+        options: ["Lovable", "Distrees", "Praise", "Genuine"],
+        answer: 3
     }
+    
+    
 ];
